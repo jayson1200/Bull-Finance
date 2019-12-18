f13f8daaa7b907ad51e7b636928592619f49591e
jayson1200 <55298583+jayson1200@users.noreply.github.com>
2019-12-17 22:31:33 -0500

    12/17/2019 at 10:28PM

---
+++ app/src/main/java/com/example/bullfinance/NetworkBridge.java
@@ -0,0 +1,63 @@
+package com.example.bullfinance;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.*;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+class NetworkBridge
+{
+    private static NetworkBridge instance;
+    private static Context ctx;
+    private RequestQueue requestQueue;
+    private ImageLoader imageLoader;
+
+    private NetworkBridge(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+
+        imageLoader = new ImageLoader(requestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized NetworkBridge getInstance(Context context) {
+        if (instance == null) {
+            instance = new NetworkBridge(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
+}
