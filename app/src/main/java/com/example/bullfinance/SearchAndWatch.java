0aa2a74ae91d0bb28c80848bb061fa23709d944c
jayson1200 <55298583+jayson1200@users.noreply.github.com>
2019-12-22 11:39:55 -0500

    12/21/2019 12:47am

--- app/src/main/java/com/example/bullfinance/SearchAndWatch.java
+++ app/src/main/java/com/example/bullfinance/SearchAndWatch.java
@@ -7,15 +7,38 @@ import android.net.ConnectivityManager;
 
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import java.lang.Object;
 
 public class SearchAndWatch extends AppCompatActivity {
+
+    public EditText usrSearch =  (EditText) findViewById(R.id.searchEditText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_and_watch);
+
+        usrSearch.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus)
+            {
+                if(hasFocus == false)
+                {
+                    usrSearch.setText("Ticker Symbol or Name");
+                }
+
+                if(hasFocus == true)
+                {
+                    usrSearch.setText("");
+                }
+            }
+        });
 
 
     }
